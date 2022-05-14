f84a7131a05c7efe9a1ba9a39475a0f91af95f10
Júlio Mota <juliomotacampos@gmail.com>
2022-05-14 17:34:05 -0300

    feat: create react app and adds page skeleton

---
+++ src/App.js
@@ -0,0 +1,283 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div class="container">
+          <div class="logo">
+            <img src="imagens/logo.png" alt="logo" />
+          </div>
+
+          <nav class="menu-desktop">
+            <ul>
+              <li><a href="/">quem sou</a></li>
+              <li><a href="/">depoimentos</a></li>
+              <li><a href="/">sobre</a></li>
+              <li><a href="/">contato</a></li>
+            </ul>
+          </nav>
+
+          <nav class="menu-mobile">
+            <ul>
+              <li><a href="/">quem sou</a></li>
+              <li><a href="/">depoimentos</a></li>
+              <li><a href="/">sobre</a></li>
+              <li><a href="/">contato</a></li>
+            </ul>
+          </nav>
+
+          <div class="clear"></div>
+        </div>
+      </header>
+
+      <section class="mosaico">
+        <div class="container">
+          <div class="mosaico-wraper">
+            <div class="mosaico-single">
+              <div class="img-mosaico-single"></div>
+              <div class="img-mosaico-single"></div>
+            </div>
+
+            <div class="mosaico-single">
+              <div class="img-mosaico-single"></div>
+              <div class="img-mosaico-single"></div>
+            </div>
+
+            <div class="mosaico-single">
+              <div class="img-mosaico-single"></div>
+              <div class="img-mosaico-single"></div>
+            </div>
+
+            <div class="mosaico-single">
+              <div class="img-mosaico-single"></div>
+              <div class="img-mosaico-single"></div>
+            </div>
+
+            <div class="mosaico-single">
+              <div class="img-mosaico-single"></div>
+              <div class="img-mosaico-single"></div>
+            </div>
+
+            <div class="mosaico-single">
+              <div class="img-mosaico-single"></div>
+              <div class="img-mosaico-single"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="servicos">
+        <div class="container">
+          <div class="box-servico">
+            <h2>Lorem ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Mauris ullamcorper.</p>
+          </div>
+
+          <div class="box-servico">
+            <h2>Lorem ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Mauris ullamcorper.</p>
+          </div>
+
+          <div class="box-servico">
+            <h2>Lorem ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Mauris ullamcorper.</p>
+          </div>
+        </div>
+      </section>
+
+      <section class="perfil-container">
+        <div class="container">
+          <div class="perfil-wraper">
+            <div class="perfil-wraper2">
+              <div class="img-perfil"></div>
+
+              <div class="texto-perfil">
+                <h2>TÍTULO DO PROFISSIONAL</h2>
+
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="videos">
+        <div class="container">
+          <div class="box-video">
+            <iframe title="video 1" src="https://www.youtube.com/embed/UhuoLV2u3u8" frameborder="0" allowfullscreen></iframe>
+          </div>
+
+          <div class="box-video">
+            <iframe title="video 1" src="https://www.youtube.com/embed/UhuoLV2u3u8" frameborder="0" allowfullscreen></iframe>
+          </div>
+
+          <div class="box-video">
+            <iframe title="video 1" src="https://www.youtube.com/embed/UhuoLV2u3u8" frameborder="0" allowfullscreen></iframe>
+          </div>
+          <div class="clear"></div>
+        </div>
+      </section>
+
+      <section class="tratamentos">
+        <h2>Tratamentos</h2>
+
+        <div class="container">
+          <div class="tratamento-container">
+            <div class="tratamento-single">
+              <h2>Profilaxia e Prevenção</h2>
+              <p>Mantenha um sorriso sem doença Periodontal, livre de cálculo, cáries e outras doenças que podem ser controladas e evitadas com a ida regular ao dentista. </p>
+            </div>
+
+            <div class="tratamento-single">
+              <h2>Profilaxia e Prevenção</h2>
+              <p>Mantenha um sorriso sem doença Periodontal, livre de cálculo, cáries e outras doenças que podem ser controladas e evitadas com a ida regular ao dentista. </p>
+            </div>
+
+            <div class="tratamento-single">
+              <h2>Profilaxia e Prevenção</h2>
+              <p>Mantenha um sorriso sem doença Periodontal, livre de cálculo, cáries e outras doenças que podem ser controladas e evitadas com a ida regular ao dentista. </p>
+            </div>
+          </div>
+
+          <div class="tratamento-container">
+            <div class="tratamento-single">
+              <h2>Profilaxia e Prevenção</h2>
+              <p>Mantenha um sorriso sem doença Periodontal, livre de cálculo, cáries e outras doenças que podem ser controladas e evitadas com a ida regular ao dentista. </p>
+            </div>
+
+            <div class="tratamento-single">
+              <h2>Profilaxia e Prevenção</h2>
+              <p>Mantenha um sorriso sem doença Periodontal, livre de cálculo, cáries e outras doenças que podem ser controladas e evitadas com a ida regular ao dentista. </p>
+            </div>
+
+            <div class="tratamento-single">
+              <h2>Profilaxia e Prevenção</h2>
+              <p>Mantenha um sorriso sem doença Periodontal, livre de cálculo, cáries e outras doenças que podem ser controladas e evitadas com a ida regular ao dentista. </p>
+            </div>
+          </div>
+
+          <div class="tratamento-container">
+            <div class="tratamento-single">
+              <h2>Profilaxia e Prevenção</h2>
+              <p>Mantenha um sorriso sem doença Periodontal, livre de cálculo, cáries e outras doenças que podem ser controladas e evitadas com a ida regular ao dentista. </p>
+            </div>
+
+            <div class="tratamento-single">
+              <h2>Profilaxia e Prevenção</h2>
+              <p>Mantenha um sorriso sem doença Periodontal, livre de cálculo, cáries e outras doenças que podem ser controladas e evitadas com a ida regular ao dentista. </p>
+            </div>
+
+            <div class="tratamento-single">
+              <h2>Profilaxia e Prevenção</h2>
+              <p>Mantenha um sorriso sem doença Periodontal, livre de cálculo, cáries e outras doenças que podem ser controladas e evitadas com a ida regular ao dentista. </p>
+            </div>
+          </div>
+
+          <div class="clear"></div>
+        </div>
+      </section>
+
+      <section class="depoimentos">
+        <h2>Depoimentos</h2>
+
+        <div class="container">
+          <div class="depoimento-single">
+            <quote>
+              “Estrutura e atendimento excepcionais! adorei o atendimento da Dra. Fernanda, excelente profissional.”
+            </quote>
+            <div class="nome-depoimento">
+              <img src="imagens/depoimento1.png" alt="depoimento" />
+              <span>Maria Augusta Santos</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="social">
+        <div class="instagram-chamada">
+          <a target="_blank" rel="noreferrer" href="http://instagram.com"><img src="imagens/inst.png" alt="instagram" /></a>
+          <p>Acesse o Instagram para ver mais fotos<br />@dra.fernanda_slovinski</p>
+        </div>
+
+        <div class="mosaico-social">
+          <div class="img-mosaico-single-social">1</div>
+          <div class="img-mosaico-single-social">2</div>
+          <div class="img-mosaico-single-social">3</div>
+          <div class="img-mosaico-single-social">4</div>
+          <div class="img-mosaico-single-social">5</div>
+          <div class="img-mosaico-single-social">6</div>
+          <div class="img-mosaico-single-social">7</div>
+          <div class="img-mosaico-single-social">8</div>
+          <div class="img-mosaico-single-social">9</div>
+        </div>
+
+        <div class="facebook-chamada">
+          <a target="_blank" rel="noreferrer" href="http://facebook.com"><img src="imagens/fb.png" alt="facebook" /></a>
+          <p>Acesse o Instagram para ver mais fotos<br />@dra.fernanda_slovinski</p>
+        </div>
+
+        <div class="clear"></div>
+      </section>
+
+      <section class="contato">
+        <h2>Contato</h2>
+
+        <form>
+          <div class="form-wraper w100">
+            <span>Nome:</span>
+            <input placeholder="" name="nome" type="text" />
+          </div>
+
+          <div class="form-wraper w50">
+            <span>E-mail:</span>
+            <input placeholder="" name="e-mail" type="text" />
+          </div>
+
+          <div class="form-wraper w50">
+            <span>Telefone:</span>
+            <input placeholder="" name="e-mail" type="text" />
+          </div>
+
+          <div class="form-wraper w100">
+            <span>Mensagem:</span>
+            <textarea placeholder=""></textarea>
+          </div>
+
+          <div class="form-wraper w100" style={{ textAlign: "center" }}>
+            <input type="submit" name="acao" value="Enviar" />
+          </div>
+
+          <div class="clear"></div>
+        </form>
+      </section>
+
+      <footer>
+        <div class="container">
+          <div class="footer-block">
+            <h2>Telefone</h2>
+            <p>(48)9999-9999<br/>(48)9999-9999</p>
+          </div>
+
+          <div class="footer-block">
+            <h2>Horários</h2>
+            <p>Segunda a sexta<br/>08:00 até 12:00</p>
+          </div>
+
+          <div class="footer-block">
+            <h2>Endereço</h2>
+            <p>Av. Trompowsky, 291. Torre 1 Sala 103<br />
+            Cep 88015300 - Florianópolis, SC. 8802001</p>
+          </div>
+
+          <div class="clear"></div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
